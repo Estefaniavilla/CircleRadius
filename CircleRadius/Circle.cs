4c26405ed49ca2aa9de650cb0bad832ba0997578
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-27 21:07:13 -0600

    Agregar archivos de proyecto.

---
+++ CircleRadius/Circle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleRadius
+{
+
+    namespace CalculateRadiusCircle
+    {
+        class Circle
+        {
+            private double radius;
+
+            
+            public Circle(double r)
+            {
+                radius = r;
+            }
+
+            
+            public double CalculateArea()
+            {
+                return Math.PI * Math.Pow(radius, 2);
+
+            }
+
+            public double CalculateCircumference()
+            {
+                return 2 * Math.PI * radius;
+            }
+
+            ~Circle()
+            {
+                Console.WriteLine("The Circle object has been destroyed.");
+            }
+        }
+    }
+}
+    
---
+++ CircleRadius/Program.cs
@@ -0,0 +1,17 @@
+﻿using CircleRadius.CalculateRadiusCircle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the radius of the circle: ");
+        double EnteredRadius = Convert.ToDouble(Console.ReadLine());
+
+        
+        Circle miCircle = new Circle(EnteredRadius);
+
+        
+        Console.WriteLine($"Circle Area: {myCircle.CalculateArea()}");
+        Console.WriteLine($"Circumference of the circle: {myCircle.CalculateCircumference()}");
+    }
+}
